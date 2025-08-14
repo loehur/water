2cd7cddac536372adce2a7d7df9e7f4588123ea1
loehur <loehur@gmail.com>
2025-08-14 21:11:42 +0700

    delete diskon334443434

--- app/Views/Rekap/main.php
+++ app/Views/Rekap/main.php
@@ -237,10 +237,6 @@ $target_page_rekap = $uri_segments[$uriCount - 1];
 
             $total_keluar += $data['prepost_cost'];
             ?>
-            <tr>
-              <td>Pre/Post Paid</td>
-              <td class="text-end"><?= number_format($data['prepost_cost']) ?></td>
-            </tr>
             <tr class="table-danger">
               <td><b>Total Pengeluaran</b></td>
               <td class="text-right"><b>Rp<?= number_format($total_keluar) ?></b></td>
